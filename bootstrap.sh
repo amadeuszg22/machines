f0926231b0fa0609cfa8b4ec3217b2a0d123dbf0
amadeuszg22 <amadeusz.grabowski@gmail.com>
2015-10-17 18:33:27 +0200

    Adding bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "System will install server features"
+
+
+echo "System Upgrades repositories"
+sudo apt-get update
+echo "System install Htop"
+sudo apt-get install htop -y
+echo "System install git & clone system configurations"
+sudo apt-get install git -y
+sudo mkdir /home/pi/machines/
+sudo git clone git@github.com:amadeuszg22/machines.git /home/pi/machines/
+echo "System copy keys"
+sudo rsync -artvpolz /home/pi/machines/root/.ssh/ /root/.ssh/
+sudo chmod a-rwx ~/.ssh/id_rsa
+sudo chmod u+rw ~/.ssh/id_rsa
+sudo "Network Configuration"
+sudo rsync -artvpolz /home/pi/machines/etc/ /etc/
